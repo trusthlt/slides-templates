446bf86ca96b98c71bcc346a7a2dc04e20693fda
Ivan Habernal <ivan.habernal@ruhr-uni-bochum.de>
2025-11-29 14:39:53 +0100

    rub fonts

--- latex-template/rub-fonts/install-rub-fonts.sh
+++ latex-template/rub-fonts/install-rub-fonts.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # We install the fonts locally into ~/.fonts/, not into /usr/share/fonts/truetype to avoid unnecessary sudo rights
-DEST_DIR="${HOME}/.fonts/rub-flama"
+# DEST_DIR="${HOME}/.fonts/rub-flama"
+DEST_DIR="/tmp/rub-flama"
 mkdir -p $DEST_DIR
 
 # Download six *.ttf files
@@ -9,8 +10,8 @@ VARIANTS='RubFlama-Bold RubFlama-BoldItalic RubFlama-Italic RubFlama-Regular Rub
 
 for VARIANT in $VARIANTS; do
   # Do not overwrite existing files
-  wget --no-clobber https://git.inf.bi.ruhr-uni-bochum.de/iib_pub/BeamerThemeIIB/-/raw/master/fonts/truetype/rub/rubflama/${VARIANT}.ttf -O "${DEST_DIR}/${VARIANT}.ttf"
+  wget --no-clobber https://git.inf.bi.ruhr-uni-bochum.de/iib_pub/BeamerThemeIIB/-/raw/master/fonts/truetype/rub/rubflama/${VARIANT}.ttf -O "${DEST_DIR}/${VARIANT}.ttf" --no-check-certificate
 done
 
 # Update font cache
-fc-cache -fv
+# fc-cache -fv
